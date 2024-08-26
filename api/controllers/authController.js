7c2e8452b0f5899d45b953c9d9dc73f8de296995
Devi krishna <devikrishna2345@gmail.com>
2024-08-26 18:50:30 +0530

    error middilware added

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/userModel.js'
 import bcrypt from 'bcryptjs'
-export const  signup = async (req, res) => {
+export const  signup = async (req, res,next) => {
    
     const {username, email, password} = req.body
     const hashedPassword =  bcrypt.hashSync(password, 10)
@@ -10,7 +10,7 @@ export const  signup = async (req, res) => {
         await newUser.save()
         res.status(201).json({message : "user created"})
     } catch (error) {
-        res.status(409).json({message : error.message})
+       next(error)
     }
 
 }
--- api/index.js
+++ api/index.js
@@ -15,4 +15,15 @@ app.use(express.json())
 app.use("/api/user", UserRoute)
 app.use("/api/auth", AuthRoute)
 
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    })
+})
+
 app.listen(3000, () => console.log('Server started on port 3000'))
