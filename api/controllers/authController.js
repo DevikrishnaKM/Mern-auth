6ea53e549fa1b93abe879e2c38263519206fe0a6
Devi krishna <devikrishna2345@gmail.com>
2024-08-27 11:18:51 +0530

    add redux toolkit

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js'
 import bcrypt from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken'
 export const  signup = async (req, res,next) => {
    
@@ -20,9 +21,9 @@ export const login = async (req, res,next) => {
     const {email, password} = req.body
     try {
         const validUser = await User.findOne({email})
-        if(!validUser) return next(createError(404, "User not found"))
+        if(!validUser) return next(errorHandler(404, "User not found"))
         const validPassword = bcrypt.compareSync(password, validUser.password)
-        if(!validPassword) return next(createError(401, "wrong credentials"))
+        if(!validPassword) return next(errorHandler(401, "wrong credentials"))
 
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
         const {password:hashedPassword, ...rest} = validUser._doc
