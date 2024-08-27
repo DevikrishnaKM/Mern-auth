fc04c2cb003714a97df5c99b22a66156ae9f833e
Devi krishna <devikrishna2345@gmail.com>
2024-08-27 14:03:54 +0530

    update header for profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Header from './components/Header'
 import Profile from './pages/Profile'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -16,7 +17,9 @@ const App = () => {
       <Route path="/about" element={<About />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/profile" element={<Profile/>} />
+      <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+      </Route>
     </Routes>
     </BrowserRouter>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+  const {currentUser} = useSelector((state) => state.user)
   return (
     <div className='bg-slate-900'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -15,8 +17,12 @@ const Header = () => {
           <Link to='/about'>
             <li className='text-white'>About</li>
           </Link>
-          <Link to='/sign-in'>
-            <li className='text-white'>Sign In</li>
+          <Link to='/profile'>
+          {currentUser ? (
+              <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+            ) : (
+              <li className='text-white'>Sign In</li>
+            )}
           </Link>
           
         </ul>
