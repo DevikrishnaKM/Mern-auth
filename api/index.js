f0c8f7b82329ca5cc8f36e5b955bdf74b6277ea5
Devi krishna <devikrishna2345@gmail.com>
2024-08-28 12:29:46 +0530

    small changes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-import path from 'path';
 import UserRoute from './routes/userRoute.js'
 import AuthRoute from './routes/authRoute.js'
 
@@ -10,15 +9,8 @@ dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(() => console.log('Connected to MongoDB')).catch(err => console.log(err))
 
-const __dirname = path.resolve();
 
 const app = express()
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  })
 
 app.use(express.json())
 
