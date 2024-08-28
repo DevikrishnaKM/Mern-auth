d7eb4129d54e0e71aa73cffc5d407f1e8fcabcd5
Devi krishna <devikrishna2345@gmail.com>
2024-08-28 11:13:19 +0530

    user updated successfully

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import UserRoute from './routes/userRoute.js'
 import AuthRoute from './routes/authRoute.js'
 
@@ -11,6 +12,8 @@ mongoose.connect(process.env.MONGO).then(() => console.log('Connected to MongoDB
 const app = express()
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use("/api/user", UserRoute)
 app.use("/api/auth", AuthRoute)
